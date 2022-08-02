2ff4046146eb9093e0d77d7bea114492cab8ec97
Your Name <you@example.com>
2022-08-03 00:41:47 +0530

    modified index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-<<<<<<< HEAD
 import { store } from './redux/store';
-=======
 import { SidebarProvider } from './context/SidebarContext';
->>>>>>> master
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
