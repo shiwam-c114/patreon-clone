7d6d8b56396b3e6db25ad56297b6bc9c02f9d7a4
damodarkolar <damodarkolar@gmail.com>
2022-08-04 22:58:56 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 import { Login } from "./pages/auth/Login";
 import { SignUp } from "./pages/auth/SignUp";
-=======
 import CreatorHome from "./pages/dashboard/Creator/CreatorHome";
->>>>>>> master
 import Home from "./pages/main/Home";
 import { AllPlansComponents } from "./pages/products/AllPlansComponents";
 import Pricing from "./pages/pricing/Pricing"
@@ -18,20 +15,14 @@ function App() {
    <EditPage/>
    <Routes>
     <Route path="/" element={<Home/>}/>
-<<<<<<< HEAD
     <Route path="/login" element={<Login/>}/>
     <Route path="/signUp" element={<SignUp/>}/>
     <Route path="/pricing" element={<Pricing/>}/>
     <Route path="/products/lite" element={<Lite/>}/>
     <Route path="/products/premium" element={<Premium/>}/>
     <Route path="/products/pro" element={<Pro/>}/>
-<<<<<<< HEAD
     <Route path="/creatorEditPage" element={<EditPage/>}/>
-=======
-=======
     <Route path="/creatorhome" element={<CreatorHome />} />
->>>>>>> master
->>>>>>> 2b3595a5a03f40fd279b164bff407939f3db1369
    </Routes>
    </>
   );
