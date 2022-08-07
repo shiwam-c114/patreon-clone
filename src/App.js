b3a92e15349ac75c558f6ee4eb2532ce32e28e34
Mohit Soni <sonimohit481.com>
2022-08-07 21:15:44 +0530

    Footer made

--- src/App.js
+++ src/App.js
@@ -10,22 +10,42 @@ import Premium from "./pages/products/Premium";
 import Pro from "./pages/products/Pro";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Video from "./pages/creators/Video";
+import Podcast from "./pages/creators/Podcast";
+import Music from "./pages/creators/Music";
+import VisualArtist from "./pages/creators/VisualArtist";
+import Community from "./pages/creators/Community";
+import Writing from "./pages/creators/Writing";
+import Gaming from "./pages/creators/Gaming";
+import Nonprofits from "./pages/creators/Nonprofits";
+import Toturial from "./pages/creators/Toturial";
+import LocalBisnesses from "./pages/creators/LocalBisnesses";
 function App() {
   return (
-   <>
-   {/* <Navbar/> */}
-   <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/login" element={<Login/>}/>
-    <Route path="/signUp" element={<SignUp/>}/>
-    <Route path="/pricing" element={<Pricing/>}/>
-    <Route path="/products/lite" element={<Lite/>}/>
-    <Route path="/products/premium" element={<Premium/>}/>
-    <Route path="/products/pro" element={<Pro/>}/>
-    <Route path="/creatorhome" element={<CreatorHome />} />
-   </Routes>
-   <Footer />
-   </>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/products/lite" element={<Lite />} />
+        <Route path="/products/premium" element={<Premium />} />
+        <Route path="/products/pro" element={<Pro />} />
+        <Route path="/creatorhome" element={<CreatorHome />} />
+        <Route path="Podcast" element={<Podcast />} />
+        <Route path="Music" element={<Music />} />
+        <Route path="Video" element={<Video />} />
+        <Route path="VisualArtist" element={<VisualArtist />} />
+        <Route path="Community" element={<Community />} />
+        <Route path="Writing" element={<Writing />} />
+        <Route path="Gaming" element={<Gaming />} />
+        <Route path="Nonprofits" element={<Nonprofits />} />
+        <Route path="Toturial" element={<Toturial />} />
+        <Route path="LocalBisnesses" element={<LocalBisnesses />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- src/components/Dashboard/CreateButton.jsx
+++ src/components/Dashboard/CreateButton.jsx
@@ -1,48 +1,54 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    useDisclosure,
-    Box
-  } from '@chakra-ui/react'
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  Box,
+} from "@chakra-ui/react";
 
-  import { FaRegEdit } from "react-icons/fa";
+// import { FaRegEdit } from "react-icons/fa";
 
+export default function CreateButton({ props }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { openBox, setOpenBox } = props;
 
-export default function CreateButton({props}) {
-    const { isOpen,onOpen, onClose } = useDisclosure()
-    const {openBox, setOpenBox} = props
-    
-    return (
-        
-      <>
-      
+  return (
+    <>
       <Box textAlign="center">
         {/* <Button leftIcon={<FaRegEdit fill='black' />} colorScheme="whiteAlpha" margin="0 20px" width="12rem"  color="black" fontSize={"sm"} textAlign="center" variant={"outline"} onClick={onOpen}>Create 2</Button> */}
       </Box>
-  
-        <Modal isOpen={openBox || isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
-            <  ModalCloseButton onClick={()=>{setOpenBox(!openBox)}} />
-            <ModalBody>
-              {/* <Lorem count={2} /> */}
-            </ModalBody>
-  
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3} onClick={()=>{onClose();setOpenBox(!openBox)}}>
-                Close
-              </Button>
-              <Button variant='ghost'>Secondary Action</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    )
-  }
+
+      <Modal isOpen={openBox || isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton
+            onClick={() => {
+              setOpenBox(!openBox);
+            }}
+          />
+          <ModalBody>{/* <Lorem count={2} /> */}</ModalBody>
+
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={() => {
+                onClose();
+                setOpenBox(!openBox);
+              }}
+            >
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
